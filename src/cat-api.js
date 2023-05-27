d16c5f03f375bae8d7ef9acd9cad47519e4f131d
Dmytro Kozlovskyi <120205405+Dmytroukraine@users.noreply.github.com>
2023-05-27 14:36:19 +0300

    fix cat

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,17 +1,48 @@
-const API_KEY =
-  'live_W2AEs8RTVOBvDUsaLZNKQrQZXkXlIHW0X4Eg712NIBm7aQnnX49SGcNBFe9bM6Js';
+// const API_KEY =
+//   'live_W2AEs8RTVOBvDUsaLZNKQrQZXkXlIHW0X4Eg712NIBm7aQnnX49SGcNBFe9bM6Js';
 
-const catApiUrl = 'https://api.thecatapi.com/v1/breeds';
-const catApiImageUrl = 'https://api.thecatapi.com/v1/images';
+// const catApiUrl = 'https://api.thecatapi.com/v1/breeds';
+// const catApiImageUrl = 'https://api.thecatapi.com/v1/images';
 
-function fetchBreeds() {
-  return fetch(`${catApiUrl}/?api_key=${API_KEY}`).then(r => r.json());
+// function fetchBreeds() {
+//   return fetch(`${catApiUrl}/?api_key=${API_KEY}`).then(r => r.json());
+// }
+
+// function fetchCatByBreed(catId) {
+//   return fetch(
+//     `${catApiImageUrl}/search?breed_ids=${catId}&api_key=${API_KEY}`
+//   ).then(response => response.json());
+// }
+
+// export { fetchBreeds, fetchCatByBreed };
+
+
+const BASE_URL = 'https://api.thecatapi.com/v1'
+const API_KEY = 'live_tOJmP1g79vmtI5Ge64CbuUmmk0rX3vegnncRIyI4DcDXMn635avs4IBSHIaAHhVk';
+
+const options = {
+  headers: {
+    'x-api-key':
+      API_KEY,
+  },
+};
+
+export function fetchBreeds() {
+  return fetch(`${BASE_URL}/breeds`, options)
+    .then(response => response.json())
+    .then(data => data.map(breed => ({ value: breed.id, name: breed.name })))
+        .catch(error => console.log(error));
+    
 }
 
-function fetchCatByBreed(catId) {
-  return fetch(
-    `${catApiImageUrl}/search?breed_ids=${catId}&api_key=${API_KEY}`
-  ).then(response => response.json());
+export function fetchCatByBreed(breedId) {
+    return fetch(`${BASE_URL}/images/search?breed_ids=${breedId}`, options)
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            console.log(data)
+            return data[0]
+        })
+    .catch(error => console.log(error))
 }
-
-export { fetchBreeds, fetchCatByBreed };
